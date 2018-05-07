c7c94626299272e45f024f6d2be51f0c7a3a911a
Marinashafiq <Marinashafiq@users.noreply.github.com>
2018-05-07 19:54:54 +0200

    project uploaded

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass= require('gulp-sass');
+
+
+gulp.task("sass",function(){
+	return gulp.src("src/scss/style.scss")
+	.pipe(sass())
+	.pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task("default",function(){
+	gulp.watch('src/scss/*.scss',['sass'])
+})
